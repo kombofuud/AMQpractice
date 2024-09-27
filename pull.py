1c4f98a0ddd2c8cb17ca3171e53f3d4bfdd0c899
kombofuud <piershanson@gmail.com>
2024-09-26 23:03:06 -0700

    savePoint before overwrite

--- pull.py
+++ pull.py
@@ -4,15 +4,18 @@ import shutil
 
 #Move downloaded file to local directory and delete previous version
 #shutil.move(r"..\..\..\Downloads\merged.json","merged.json")
-
-#load songs from each list
-#fileMerged = "merged"
-#fileList = ["0","10","20","30","40","50","60","70","80","90","100","last"]
-#fileLoad = "loading"
+'''
+load songs from each list
+fileMerged = "merged"
+fileList = ["0","10","20","30","40","50","60","70","80","90","100","last"]
+fileLoad = "loading"
+fileDead = "dead"
+'''
 fileLearned = "learned"
 fileMerged = "dummyMerged"
 fileList = ["dummy0", "dummy1"]
 fileLoad = "dummyLoad"
+fileDead = "dummyDead"
 
 with open(fileMerged+".json", 'r', encoding = 'utf8') as f:
     songList = json.load(f)
@@ -20,16 +23,23 @@ with open(fileLoad+".json", 'r', encoding = 'utf8') as f:
     loadingList = json.load(f)
 with open("modifications.json", 'r', encoding = 'utf8') as f:
     equivalances = json.load(f)
+with open(fileDead+".json", 'r', encoding = 'utf8') as f:
+    deadList = json.load(f)
 
 #load equivalence mapping
 equiv = dict()
 index = 0
 altNames = []
+deadSongs = []
+deadSongMap = set()
 for relation in equivalances:
     for name in relation["equiv"]:
         equiv[name] = index
     index += 1
     altNames.append(set())
+
+for song in deadList:
+    deadSongMap.add(song["video720"])
 
 #add missing info to songs
 songCodes = dict()
@@ -78,6 +88,9 @@ for section in fileList:
             index = index+1
         else:
             knownList.pop(index)
+            if song["video720"] not in deadSongMap:
+                deadSongMap.add(song["video720"])
+                deadSongs.append(song)
     with open(section+"cutlist.json", 'w', encoding = 'utf8') as f:
         f.truncate(0)
         f.seek(0)
@@ -153,6 +166,19 @@ with open(fileLoad+".json", "r+", encoding = 'utf8') as f:
     f.seek(0)
     f.write(fileData)
 
+with open(fileDead+".json", "r+", encoding = 'utf8') as f:
+    knownList = json.load(f)
+    knownList.extend(deadSongs)
+    f.truncate(0)
+    f.seek(0)
+    json.dump(knownList,f)
+    f.seek(0)
+    fileData = f.read()
+    fileData = fileData.replace(", {","\n,{")
+    fileData = fileData.replace("}]","}\n]")
+    f.seek(0)
+    f.write(fileData)
+
 with open(fileMerged+".json", "r+", encoding = 'utf8') as f:
     f.truncate(0)
     json.dump(songList,f)
@@ -165,9 +191,12 @@ with open(fileMerged+".json", "r+", encoding = 'utf8') as f:
 
 
 #Print out new songs
-print("newLearning:")
+print("newLearning:-----------------------------------")
 for song in newKnownList:
     print(song["animeEnglishName"]+": "+song["songName"]+" by "+song["songArtist"])
-print("newLoading:")
-for song in newKnownList:
+print("newLoading:-----------------------------------")
+for song in newLoadingList:
+    print(song["animeEnglishName"]+": "+song["songName"]+" by "+song["songArtist"])
+print("Deprecated:-----------------------------------")
+for song in deadSongs:
     print(song["animeEnglishName"]+": "+song["songName"]+" by "+song["songArtist"])
