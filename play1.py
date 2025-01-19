a7856f9f4caf2e3382c84c15c91c5417192fb0d2
kombofuud <piershanson@gmail.com>
2025-01-19 00:51:16 -0800

    updated play1 for upcoming changes

--- play1.py
+++ play1.py
@@ -10,147 +10,134 @@ import numpy
 '''
 fileMerged = "merged.json"
 fileDead = "dead.json"
-fileLearned = "learnedcutlist.json"
 filePrep = "preplist.json"
 fileLoad = "loadingcutlist.json"
 fileList = ["0cutlist.json", "10cutlist.json", "20cutlist.json", "30cutlist.json", "40cutlist.json", "50cutlist.json", "60cutlist.json", "70cutlist.json", "80cutlist.json", "90cutlist.json", "100cutlist.json", "lastcutlist.json"]
 '''
 fileMerged = "dummyMerged.json"
 fileDead = "dummyDead.json"
-fileLearned = "dummyLearnedcutlist.json"
 filePrep = "dummyPreplist.json"
 fileLoad = "dummyLoad.json"
 fileList = ["dummy0cutlist.json", "dummy1cutlist.json"]
 
-equivalences = "modifications.json"
-newURLs = "broken.json"
+#Get list of songs in pool
+songWeightMap = map()
+listWeights = [0]
+songCounter = 0
+with open(fileList[0], "r", encoding="utf-8") as file:
+    poolSongList = json.load(file)
+    for song in poolSongList:
+        songWeightMap[song["annSongId"]] = song["D"]
+        songPlaceMap[song["annSongId"]] = songCounter
+        listWeights[0] += song["D"]
+        songCounter += 1
 
-#Make list of all songs
-with open(fileMerged, "r", encoding="utf-8") as file:
-    fullSongList = json.load(file)
-songMap = {}
-songCounter = []
-uniqueSongCounter = []
-for index, song in enumerate(fullSongList):
-    songMap[song["annSongId"]] = index
-    songCounter.append(0)
-    uniqueSongCounter.append(0)
-
-#Read all relevant files and count song statistics for those files
-for file in fileList:
-    with open(file, "r", encoding="utf-8") as file:
-        localSongSet = set()
-        localSongList = json.load(file)
-        for index, song in enumerate(localSongList):
-            songCounter[songMap[song["annSongId"]]] += 1
-            if song["annSongId"] not in localSongSet:
-                localSongSet.add(song["annSongId"])
-                uniqueSongCounter[songMap[song["annSongId"]]] += 1
-loadCounts = []
-for file in [fileLoad, filePrep]:
+#Read all relevant files and get the song weight total for those files:
+for file in fileList[1:]:
     with open(file, "r", encoding="utf-8") as file:
         localSongList = json.load(file)
-        loadCounts.append(len(localSongList))
-        for index, song in enumerate(localSongList):
-            songCounter[songMap[song["annSongId"]]] += 1
-
-#update LearnedcutList and mark hard songs
-learnedList = []
-hardList = []
-hardCounter = 0
-hardSet = set()
-for song in fullSongList:
-    if songCounter[songMap[song["annSongId"]]] == 0:
-        learnedList.append(song)
-        continue
-    if songCounter[songMap[song["annSongId"]]]-uniqueSongCounter[songMap[song["annSongId"]]] > 8:
-        hardList.append(song)
-        hardSet.add(song["annSongId"])
-        hardCounter += uniqueSongCounter[songMap[song["annSongId"]]]
+        listWeights.append(0)
+        for song in localSongList:
+            songWeightMap[song["annSongId"]] += song["D"]
+            listWeights[-1] += song["D"]
 
 #Get new songs from preplist
-songMean = (sum(songCounter)-sum(loadCounts)-hardCounter)/len(fileList)
+songMean = (sum(listWeights))/songCounter
 targetSongMean = 530
-targetPrepCount = 70
-newSongList = []
+targetPrepCount = 2
 
-if songMean < targetSongMean or len(hardList):
+if songMean < targetSongMean:
     newCount = max(0,math.ceil(targetSongMean-songMean))
 
+#pick new preplist songs
     with open(filePrep, "r", encoding="utf-8") as file:
         prepList = json.load(file)
         newCount = min(newCount,len(prepList)
-        newSongList.extend(prepList[:newCount])
-        prepList = prepList[newCount:].extend(hardList)
-        newPrepSongCount = targetPrepCount-len(prepList)
-    fullSongList.extend(newSongList)
+        newSongList = prepList[:newCount]
+        prepList = prepList[newCount:]
+        songMean += newCount
+        newPrepSongCount = max(0,targetPrepCount-len(prepList))
+    poolSongList.extend(newSongList)
     for index, song in enumerate(newSongList):
-        songMap[song["annSongId"]] = index+len(fullSongList)
-        songCounter.append(len(fileList))
-        uniqueSongCounter.append(len(fileList))
+        songWeightMap[song["annSongId"]] = len(fileList)
+        songCounter += 1
 
+#add new songs to preplist if needed
     with open(fileLoad, "r+", encoding="utf-8") as file:
         loadingList = random.shuffle(json.load(file))
-        newSongList.append(loadingList[:newPrepSongCount])
+        prepList.append(loadingList[:newPrepSongCount])
         loadingList = loadingList[newCount:]
         f.truncate(0)
         f.seek(0)
-        json.dump(loadingList,f,ensure_ascii=False)
+        json.dump(loadingList,file,ensure_ascii=False)
         f.seek(0)
         fileData = f.read()
         fileData = fileData.replace(", {","\n,{")
         fileData = fileData.replace("}]","}\n]")
         f.seek(0)
         f.write(fileData)
-        
+
     with open(filePrep, "w", encoding="utf-8") as file:
         f.truncate(0)
         f.seek(0)
-        json.dump(newSongList,f,ensure_ascii=False)
+        json.dump(prepList,file,ensure_ascii=False)
         f.seek(0)
         fileData = f.read()
         fileData = fileData.replace(", {","\n,{")
         fileData = fileData.replace("}]","}\n]")
         f.seek(0)
         f.write(fileData)
+#add new songs to practice lists
+    for file in fileList:
+        with open(file, "r+", encoding="utf-8") as file:
+            localSongList = json.load(file)
+            file.extend(newSongList)
+            localSongList = random.shuffle(localSongList)
+            f.truncate(0)
+            f.seek(0)
+            json.dump(localSongList,file,ensure_ascii=False)
+            f.seek(0)
+            fileData = f.read()
+            fileData = fileData.replace(", {","\n,{")
+            fileData = fileData.replace("}]","}\n]")
+            f.seek(0)
+            f.write(fileData)
         
-
-#Remove hard songs and add new songs
-songCounts = []
-for file in fileList:
-    with open(file, "r+", encoding="utf-8") as file:
-        localSongList = json.load(file)
-        index = 0
-        while index < len(localSongList):
-            if localSongList[index]["annSongId"] in hardSongSet:
-                localSongList.pop(index)
-                continue
-            index += 1
-        localSongList.extend(newSongList) 
-        songCounts.extend(len(localSongList))
-        random.shuffle(localSongList)
-        f.truncate(0)
-        f.seek(0)
-        json.dump(localSongList,f,ensure_ascii=False)
-        f.seek(0)
-        fileData = f.read()
-        fileData = fileData.replace(", {","\n,{")
-        fileData = fileData.replace("}]","}\n]")
-        f.seek(0)
-        f.write(fileData)
-
 #Pick List and Calculate Song Probabilities
 
 #picking list
-songMean = statistics.mean(songCounts)
-songDeviation = statistics.pstdev(zScores)
+songDeviation = statistics.pstdev(listWeights)
 zScores = []
-for val in songCounts:
+for val in listWeights:
     zScores.append(exp((val-songMean)/songDeviation))
-quizList = random.choices(fileList, weights = zScores)
+quizList = random.choices(fileList, weights = zScores)[0]
 
 #adjusting song probabilities
+weightList = []
+with open(quizList, "r", encoding="utf-8") as file:
+    localSongList = json.load(quizList)
+    for song in localSongList:
+        songWeightMap[song["annSongId"]] += song["D"]
+    for song in poolSongList:
+        weightList.append(exp(songWeightMap[song["annSongId"]]))
+weightListSum = sum(weightList)
+weightList = [weight/weightListSum for weight in weightList]
 
 #Pick Songs, write _quiz, and erase _practice
+songCount = min(30,len(poolSongList))
+randomSongList = numpy.random.choice(poolSongList, size = songCount, p = weightList, replace = False)
+
+with open("_quiz.json", 'w', encoding = 'utf8') as f:
+    json.dump(randomSongList, f)
+with open("_practice.json", 'w', encoding = 'utf8') as f:
+    f.write("[{}\n]")
 
 #Text Output
+print()
+print(quizList)
+if len(newSongList):
+    print("Added Songs-----------------------")
+    for song in newSongList:
+        print(song["animeEnglishName"]+": "+song["songName"]+" by "+song["songArtist"])
+print()
+print("Pool Size: "+songCounter)
