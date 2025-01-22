e663fc00920e693573b6e9601857c325dc91d49f
kombofuud <piershanson@gmail.com>
2025-01-22 01:58:22 -0800

    finished stats.py

--- stats.py
+++ stats.py
@@ -25,10 +25,12 @@ loadingSize += len(localList)
 #get list statistics
 listDistribution = []
 progressCounter = map()
-songMean = map()
+songWeight = map()
+weightMax = -99
+weightMin = 99
 instanceCount = map()
-instanceMax = -100
-instanceMin = 100
+instanceMax = -99
+instanceMin = 99
 for file in fileList:
     with open(file, 'r', encoding = 'utf8') as f:
         localList = json.load(f)
@@ -43,15 +45,30 @@ for file in fileList:
                 songMean[song["annSongId"]] = song["D"]
             else:
                 songMean[song["annSongId"]] += song["D"]
+                if file == fileList[-1]:
+                    weightMax = max(weightMax, songMean[song["annSongId"]])
+                    weightMin = min(weightMin, songMean[song["annSongId"]])
             if song["D"] not in instanceCount:
                 instanceCount[song["D"]] = 1
                 instanceMax = max(song["D"],instanceMax)
                 instanceMin = min(song["D"],instanceMin)
             else:
                 instanceCount[song["D"]] += 1
+#get frequency of various statistics
+progressFrequency = [0]*len(fileList)
+#weightCounter = [0]*(weightMax-weightMin+1)
+#instanceCounter = [0]*(instanceMax-instanceMin+1)
+for key in progressCounter.keys():
+    progressFrequency[progressCounter[key]] += 1
+#    weightCounter[songWeight[key]-weightMin] += 1
+#    instanceCounter[instanceCount[key]-instanceMin] += 1
 
 #print statistics
 print("Pool Size: "+poolSize+" LoadingSize: "+loadingSize+" MeanSize: "+round(sum(listDistribution)/len(fileList),2)
 print()
+print(" 0   10  20  30  40  50  60  70  80  90  100 Last")
 print(listDistribution)
 print()
+print(progressFrequency)
+print()
+print("MinWeight: "+weightMin+" MaxWeight: "+weightMax+"LocalMinWeight: "+instanceMin+"LocalWeightMax: "+instanceMax)
