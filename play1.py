f2ce26ed94209009ee57cc11c4e56584ba3d9775
kombofuud <piershanson@gmail.com>
2025-01-23 22:43:59 -0800

    fixed play1.py

--- play1.py
+++ play1.py
@@ -9,7 +9,6 @@ import numpy
 #Read ALL Files
 '''
 fileMerged = "merged.json"
-fileDead = "dead.json"
 filePrep = "preplist.json"
 fileLoad = "loadingcutlist.json"
 fileList = ["0cutlist.json", "10cutlist.json", "20cutlist.json", "30cutlist.json", "40cutlist.json", "50cutlist.json", "60cutlist.json", "70cutlist.json", "80cutlist.json", "90cutlist.json", "100cutlist.json", "lastcutlist.json"]
@@ -18,7 +17,6 @@ targetPrepCount = 2
 desiredQuizSize = 30
 '''
 fileMerged = "dummyMerged.json"
-fileDead = "dummyDead.json"
 filePrep = "dummyPreplist.json"
 fileLoad = "dummyLoad.json"
 fileList = ["dummy0cutlist.json", "dummy1cutlist.json"]
@@ -34,7 +32,7 @@ with open(fileList[0], "r", encoding="utf-8") as file:
     poolSongList = json.load(file)
     for song in poolSongList:
         songWeightMap[song["annSongId"]] = song["D"]
-        songPlaceMap[song["annSongId"]] = songCounter
+        #songPlaceMap[song["annSongId"]] = songCounter
         listWeights[0] += song["D"]
         songCounter += 1
 
@@ -56,57 +54,58 @@ if songMean < targetSongMean:
 #pick new preplist songs
     with open(filePrep, "r", encoding="utf-8") as file:
         prepList = json.load(file)
-        newCount = min(newCount,len(prepList)
+        newCount = min(newCount,len(prepList))
         newSongList = prepList[:newCount]
         prepList = prepList[newCount:]
         songMean += newCount
         newPrepSongCount = max(0,targetPrepCount-len(prepList))
     poolSongList.extend(newSongList)
-    for index, song in enumerate(newSongList):
+    for song in newSongList:
         songWeightMap[song["annSongId"]] = len(fileList)
         songCounter += 1
 
 #add new songs to preplist if needed
     with open(fileLoad, "r+", encoding="utf-8") as file:
-        loadingList = random.shuffle(json.load(file))
-        prepList.append(loadingList[:newPrepSongCount])
-        loadingList = loadingList[newCount:]
-        f.truncate(0)
-        f.seek(0)
+        loadingList = json.load(file)
+        random.shuffle(loadingList)
+        prepList.extend(loadingList[:newPrepSongCount])
+        loadingList = loadingList[newPrepSongCount:]
+        file.truncate(0)
+        file.seek(0)
         json.dump(loadingList,file,ensure_ascii=False)
-        f.seek(0)
-        fileData = f.read()
+        file.seek(0)
+        fileData = file.read()
         fileData = fileData.replace(", {","\n,{")
         fileData = fileData.replace("}]","}\n]")
-        f.seek(0)
-        f.write(fileData)
+        file.seek(0)
+        file.write(fileData)
 
-    with open(filePrep, "w", encoding="utf-8") as file:
-        f.truncate(0)
-        f.seek(0)
+    with open(filePrep, "r+", encoding="utf-8") as file:
+        file.truncate(0)
+        file.seek(0)
         json.dump(prepList,file,ensure_ascii=False)
-        f.seek(0)
-        fileData = f.read()
+        file.seek(0)
+        fileData = file.read()
         fileData = fileData.replace(", {","\n,{")
         fileData = fileData.replace("}]","}\n]")
-        f.seek(0)
-        f.write(fileData)
+        file.seek(0)
+        file.write(fileData)
 
 #add new songs to practice lists
     for file in fileList:
         with open(file, "r+", encoding="utf-8") as file:
             localSongList = json.load(file)
-            file.extend(newSongList)
-            localSongList = random.shuffle(localSongList)
-            f.truncate(0)
-            f.seek(0)
+            localSongList.extend(newSongList)
+            random.shuffle(localSongList)
+            file.truncate(0)
+            file.seek(0)
             json.dump(localSongList,file,ensure_ascii=False)
-            f.seek(0)
-            fileData = f.read()
+            file.seek(0)
+            fileData = file.read()
             fileData = fileData.replace(", {","\n,{")
             fileData = fileData.replace("}]","}\n]")
-            f.seek(0)
-            f.write(fileData)
+            file.seek(0)
+            file.write(fileData)
 
 #Pick List and Calculate Song Probabilities
 
@@ -114,24 +113,24 @@ if songMean < targetSongMean:
 songDeviation = statistics.pstdev(listWeights)
 zScores = []
 for val in listWeights:
-    zScores.append(exp((val-songMean)/songDeviation))
+    zScores.append(math.exp((val-songMean)/songDeviation))
 quizList = random.choices(fileList, weights = zScores)[0]
-print(zScores)
 
 #adjusting song probabilities
 weightList = []
 with open(quizList, "r", encoding="utf-8") as file:
-    localSongList = json.load(quizList)
+    file.seek(0)
+    localSongList = json.load(file)
     for song in localSongList:
         songWeightMap[song["annSongId"]] += song["D"]
     for song in poolSongList:
-        weightList.append(exp(songWeightMap[song["annSongId"]]))
+        weightList.append(math.exp(songWeightMap[song["annSongId"]]))
 weightListSum = sum(weightList)
 weightList = [weight/weightListSum for weight in weightList]
 
 #Pick Songs, write _quiz, and erase _practice
 songCount = min(desiredQuizSize,len(poolSongList))
-randomSongList = numpy.random.choice(poolSongList, size = songCount, p = weightList, replace = False)
+randomSongList = list(numpy.random.choice(poolSongList, size = songCount, p = weightList, replace = False))
 
 with open("_quiz.json", 'w', encoding = 'utf8') as f:
     json.dump(randomSongList, f)
@@ -141,9 +140,10 @@ with open("_practice.json", 'w', encoding = 'utf8') as f:
 #Text Output
 print()
 print(quizList)
+print()
 if len(newSongList):
     print("Added Songs-----------------------")
     for song in newSongList:
         print(song["animeEnglishName"]+": "+song["songName"]+" by "+song["songArtist"])
 print()
-print("Pool Size: "+songCounter)
+print("Pool Size: "+str(songCounter))
