b1f262a90081354aec2e1323ac3d236615c0b12c
kombofuud <piershanson@gmail.com>
2025-07-27 13:42:18 -0700

    compile.py now uses the same sampling distribution as play.py

--- compile.py
+++ compile.py
@@ -83,10 +83,11 @@ for i, song in enumerate(songPool):
                 pSong = copy.deepcopy(song)
                 sectionCount = len(pSong["sampleWeights"])-2
                 pSong["sampleWeights"][math.ceil(quizSamples[pSong["ID"]]*sectionCount/100*1.0000001)] += quizIds[pSong["ID"]]
-                if song["D"] < 3:
-                    pSong["startPoint"] = quizSamples[song["ID"]]
-                else:
-                    pSong["startPoint"] = song["sampleWeights"]
+                #adjust song weight strength to match play weights
+                songWeightStrength = 1-math.pow(0.95,18-pSong["D"])
+                for i in range(len(psong["sampleWeights"])):
+                    pSong["sampleWeights"][i] = math.pow(len(pSong["sampleWeights"]),pSong["sampleWeights"][i]*songWeightStrength)
+                pSong["startPoint"] = pSong["sampleWeights"]
                 practice.append(pSong)
             elif song["X"] != 0:
                 print(f"QuizSong Status Val Undefined: ANNID={song["ID"]}, {song["SN"]} _from_ {song["EN"]}")
