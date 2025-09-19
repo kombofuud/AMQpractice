a0a02196dbb4e10a75c9fe3bf98d55aed7c1bd04
kombofuud <piershanson@gmail.com>
2025-09-19 01:25:53 -0700

    added program to scan a downloaded completed list and list shows that need to be added and removed from completed

---
+++ scan.py
@@ -0,0 +1,30 @@
+import json
+import random
+
+with open("pool.json", 'r', encoding = 'utf8') as f:
+    known = json.load(f)
+with open("preplist.json", 'r', encoding = 'utf8') as f:
+    learning = json.load(f)
+    learning.pop(0)
+with open("loadingcutlist.json", 'r', encoding = 'utf8') as f:
+    learning.extend(json.load(f))
+
+with open("../../../Downloads/merged.json", 'r', encoding = 'utf8') as f:
+    playPool = json.load(f)
+
+playMalIds = set()
+for song in playPool:
+    playMalIds.add(song["malId"])
+
+learningMalIds = set()
+print("unfinished MALIDs:__________________")
+for song in learning:
+    if song["malId"] in playMalIds:
+        print(song["malId"]
+    learningMalIds.add(song["malId"])
+
+print("unadded MALIDs:_____________________")
+for song in known:
+    if song["malId"] not in playMalIds and song["malId"] not in learningMalIds:
+        print(song["malId"])
+        playMalIds.add(song["malId"])
