79a2e4ec4e8c37f520b2afa510808a435e26d26e
kombofuud <piershanson@gmail.com>
2025-12-11 23:28:34 -0800

    changed core programs for new change, preparing to change data

--- findMean.py
+++ findMean.py
@@ -7,30 +7,29 @@ with open("pool.json", 'r', encoding = 'utf8') as f:
     known = json.load(f)
 
 DList = []
-DMin = 9999
-DMax = 10
+DMax = -1
 indexMap = dict()
 maxWeightCount = 0
 songCounter = 0
 for song in known:
-    if song["D"] == DMax:
+    if song["D"] == 0:
         maxWeightCount += 1
-    DList.append(math.exp(song["D"])*math.sqrt(len(song["sampleWeights"])))
-    DMin = min(DMin, song["D"])
+    DList.append(math.exp(-song["D"])*math.sqrt(len(song["sampleWeights"])))
+    DMax = max(DMax, song["D"])
     songCounter += 1
 
-minCount = 9999999999
+minCount = len(DList)+1
 maxCount = -1
 songTotal = 0
-songDistribution = [0]*(DMax-DMin+1)
+songDistribution = [0]*(DMax+1)
 for _ in range(1000):
     randomSongList = list(numpy.random.choice(known, size = len(DList), p = DList/numpy.sum(DList), replace = False))
     songCount = 0
     testWeightCount = maxWeightCount
     for song in randomSongList:
         songCount += 1
-        songDistribution[DMax-song["D"]] += 1
-        if song["D"] == DMax:
+        songDistribution[song["D"]] += 1
+        if song["D"] == 0:
             testWeightCount-= 1
         if testWeightCount == 0:
             break
