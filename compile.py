75270b5f433b610f132d7ab9a4a030f724dcf2b4
kombofuud <piershanson@gmail.com>
2026-04-13 02:06:02 -0700

    trying different weight distributions

--- compile.py
+++ compile.py
@@ -83,7 +83,7 @@ prevWeightCount = 0
 prevNewSongs = 0
 
 for i, song in enumerate(songPool):
-    prevWeightCount += 1/math.sqrt(math.gamma(1.5+song["D"]))
+    prevWeightCount += 1/(1+4**(song["D"]-3))
     song["SN"] = f"`{song["songName"]}`"
     if song["D"] == 0 and type(song["D"]) is int:
         prevNewSongs += 1
@@ -198,13 +198,13 @@ phantomWeightCount = 0
 songDistribution = [0]*(maxD-minD+1)
 for song in songPool:
     songDistribution[int(round(song["D"]-minD))] += 1
-    phantomWeightCount += 1/math.sqrt(math.gamma(1.5+song["D"]))
+    phantomWeightCount += 1/(1+4**(song["D"]-3))
     song["D"] = max(song["D"], 0.0)
-    currentWeightCount += 1/math.sqrt(math.gamma(1.5+song["D"]))
+    currentWeightCount += 1/(1+4**(song["D"]-3))
 
 with open(gainFile, 'r', encoding = 'utf8') as f:
     targetMean, oldWeight, prevGain = [float(line.strip()) for line in f.readlines()]
-weightChange = 40.5-currentWeightCount
+weightChange = 60.5-currentWeightCount
 
 #targetGain = min(2, max(-2, (weightChange-prevGain)/(prevWeightCount-oldWeight)/20))
 targetGain = 0
